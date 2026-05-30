d45045e777f9551e58c21f3020451c2c963d2ea7
Gibbsite <113031236+Gibbsite@users.noreply.github.com>
2026-05-30 03:54:14 +0300

    Add files via upload

--- Scripts/activate.bat
+++ Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=d:\NURE\Dimon\3 course\2_semester\DevOps\PZ1\.venv
+set VIRTUAL_ENV=d:\NURE\Dimon\3 course\2_semester\DevOps\PZ1\medium_level\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
